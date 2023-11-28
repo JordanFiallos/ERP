2d9184bf39fc3b55bd442103b9a51416cb1a5909
Ruben <martinez.bueno.ruben@alumnat.copernic.cat>
2023-11-28 16:42:15 +0100

    RF01 Implementado falta poner BBDD y vista pagina Login con thymeleaf agregada

---
+++ InitializrSpringbootProject/src/main/java/com/pet_it/program/DAO/supplierDAO.java
@@ -0,0 +1,14 @@
+package com.pet_it.program.DAO;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Jordan
+ */
+public interface supplierDAO {
+    
+}
