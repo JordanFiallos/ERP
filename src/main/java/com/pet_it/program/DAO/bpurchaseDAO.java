c1cf160910ee5f034d366a967a0b342ab89d2cb5
Ruben <martinez.bueno.ruben@alumnat.copernic.cat>
2023-12-27 17:09:06 +0100

    RF013 - Hecho

--- src/main/java/com/pet_it/program/DAO/bpurchaseDAO.java
+++ src/main/java/com/pet_it/program/DAO/bpurchaseDAO.java
@@ -4,6 +4,7 @@
  */
 package com.pet_it.program.DAO;
 
+import com.pet_it.program.domain.bBill;
 import com.pet_it.program.domain.bPurchase;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +16,26 @@ import org.springframework.data.jpa.repository.Query;
  * @author Ricard
  */
 public interface bpurchaseDAO extends JpaRepository<bPurchase, Long> {
-    @Query(value="SELECT * FROM b_bill WHERE dtype = \"Purchase\" AND scheduled_delivery_date = ?1 OR scheduled_delivery_date > ?1",nativeQuery=true)
+
+    @Query("SELECT COUNT(*) FROM bPurchase b")
+    int listaFacturasCompra();
+
+    @Query("SELECT COUNT(*) FROM bPurchase b WHERE b.semana = ?1 AND YEAR(b.scheduledDaliveryDate) = ?2")
+    int listaComprasSemana(int semana, int año);
+
+    @Query("SELECT SUM(b.total) FROM bPurchase b")
+    int sumaTotals();
+
+    @Query("SELECT SUM(b.total) FROM bPurchase b WHERE b.semana = ?1 AND YEAR(b.scheduledDaliveryDate) = ?2")
+    Integer sumaTotalsSemana(int semana, int año);
+
+    @Query("SELECT SUM(b.quantity) FROM bPurchase b")
+    int sumaCantidad();
+
+    @Query("SELECT SUM(b.quantity) FROM bPurchase b WHERE b.semana = ?1 AND YEAR(b.scheduledDaliveryDate) = ?2")
+    int sumaCantidadSemana(int semana, int año);
+
+    @Query(value = "SELECT * FROM b_bill WHERE dtype = \"Purchase\" AND scheduled_delivery_date = ?1 OR scheduled_delivery_date > ?1", nativeQuery = true)
     List<bPurchase> listPurchasesSinceDate(LocalDateTime fecha);
+
 }
---
+++ src/main/java/com/pet_it/program/services/bBillService.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pet_it.program.services;
+
+import com.pet_it.program.domain.bBill;
+import com.pet_it.program.domain.bPurchase;
+import com.pet_it.program.domain.bSell;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ *
+ * @author Ruben
+ */
+
+public interface bBillService {
+    
+    public List<bBill> getAllbBills();
+    
+    public List<bPurchase> getAllbPurchases();
+        
+    public int getAllPurchases();
+    
+    public int comprasPorSemana(int semana,int año);
+    
+    public int sumaTotals();
+    
+    public Integer sumaTotalsSemana(int semana,int año);
+    
+    public int sumaCantidades();
+    
+    public int sumaCantidadesSemana(int semana,int año);
+    
+    public List<bSell> getAllbSells();
+    
+    public int listaVentas();
+    
+    public int ventasPorSemana(int semana,int año);
+    
+    public int ventasTotals();
+    
+    public Integer ventasTotalsSemana(int semana,int año);
+    
+    public int ventasCantidades();
+    
+}
